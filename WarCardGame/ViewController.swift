1e2008ca7b3bc95a18a849272161e48c3606b0c7
19AF OSS <mark.thrower.2@us.af.mil>
2019-07-29 11:40:53 -0500

    Add images

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var LeftIageView: UIImageView!
+    
+    @IBOutlet weak var RightImageView: UIImageView!
+    
+    @IBOutlet weak var LeftScoreLabel: UILabel!
+    
+    @IBOutlet weak var RightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        LeftIageView.image = UIImage(named: "card\(leftNumber)")
+        RightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            leftScore += 1
+        LeftScoreLabel.text = String(leftScore)
+        }
+        
+        else if leftNumber < rightNumber {
+            rightScore += 1
+            RightScoreLabel.text = String(rightScore)
+        }
+        else {
+            
+        }
+    }
+    
+    
+    
 }
 
